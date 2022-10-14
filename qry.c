b73bfe77f34833da0843d4dbc8d02dc3efe75cea
vfs <vischijau@gmail.com>
2022-10-14 18:17:27 -0300

    Basic implementation of e function

--- qry.c
+++ qry.c
@@ -1,26 +1,51 @@
 #include "qry.h"
 
-void funcE(SRbTree tree, FILE* qry, FILE* qrytxt)
+#include "forms.h"
+
+/* Functions and structs for E */
+struct e
+{
+    double v;
+    FILE* qrytxt;
+};
+
+void energize(Info i, double x, double y, double mbbX1, double mbbY1, double mbbX2, double mbbY2, void* aux)
+{
+    struct e* e = aux;
+    fprintf(e->qrytxt, "Energized %s with id %d and energy %lf", formTypeToString(getFormType(i)), getFormId(i), e->v);
+    setNauEnergy(i, e->v);
+}
+
+void e(SRbTree tree, FILE* qry, FILE* qrytxt)
+{
+    struct e* aux = calloc(1, sizeof(struct e));
+    aux->qrytxt = qrytxt;
+    fscanf(qry, "%lf", &aux->v);
+    fprintf(qrytxt, "Energizing all vessels with %lf:\n", aux->v);
+    percursoProfundidade(tree, energize, aux);
+    free(aux);
+}
+
+/* Functions and structs for MV */
+void mv(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
 
 }
 
-void funcMV(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
+/* Functions and structs for LR */
+void lr(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
 
 }
 
-void funcLR(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
+/* Functions and structs for D */
+void d(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
 
 }
 
-void funcD(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
-{
-
-}
-
-void funcMC(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
+/* Functions and structs for MC */
+void mc(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
 
 }
--- qry.h
+++ qry.h
@@ -5,14 +5,14 @@
 #ifndef QRY_H
 #define QRY_H
 
-void funcE(SRbTree tree, FILE* qry, FILE* qrytxt);
+void e(SRbTree tree, FILE* qry, FILE* qrytxt);
 
-void funcMV(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
+void mv(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
 
-void funcLR(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
+void lr(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
 
-void funcD(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
+void d(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
 
-void funcMC(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
+void mc(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg);
 
 #endif
