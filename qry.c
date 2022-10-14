f2b085e8c5878a8c881cf7f5af475edbcc831a24
vfs <vischijau@gmail.com>
2022-10-14 18:34:36 -0300

    Changed txt reports of e function

--- qry.c
+++ qry.c
@@ -12,7 +12,7 @@ struct e
 void energize(Info i, double x, double y, double mbbX1, double mbbY1, double mbbX2, double mbbY2, void* aux)
 {
     struct e* e = aux;
-    fprintf(e->qrytxt, "Energized %s with id %d and energy %lf", formTypeToString(getFormType(i)), getFormId(i), e->v);
+    fprintf(e->qrytxt, "Energized %s id %d with energy %.2lf\n", formTypeToString(getFormType(i)), getFormId(i), e->v);
     setNauEnergy(i, e->v);
 }
 
@@ -21,7 +21,7 @@ void e(SRbTree tree, FILE* qry, FILE* qrytxt)
     struct e* aux = calloc(1, sizeof(struct e));
     aux->qrytxt = qrytxt;
     fscanf(qry, "%lf", &aux->v);
-    fprintf(qrytxt, "Energizing all vessels with %lf:\n", aux->v);
+    fprintf(qrytxt, "\n>Energizing all vessels with %.2lf:\n", aux->v);
     percursoProfundidade(tree, energize, aux);
     free(aux);
 }
