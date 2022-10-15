1cf69356ff7be63852c9bd0edbd23290586e2923
vfs <vischijau@gmail.com>
2022-10-15 18:00:35 -0300

    Implementation of d funtion

--- qry.c
+++ qry.c
@@ -118,15 +118,15 @@ struct lr
     double w;
     double h;
     char side[3];
-    FILE* qrysvg;
     FILE* qrytxt;
+    FILE* qrysvg;
 };
 
 void lr(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
     struct lr* aux = calloc(1, sizeof(struct lr));
-    aux->qrysvg = qrysvg;
     aux->qrytxt = qrytxt;
+    aux->qrysvg = qrysvg;
     fscanf(qry, "%d %s %lf %lf %lf", &aux->id, aux->side, &aux->d, &aux->w, &aux->h);
 
     free(aux);
@@ -138,17 +138,89 @@ struct d
     int id;
     char side[3];
     double d;
-    FILE* qrysvg;
+    double shootX;
+    double shootY;
+    bool hit;
+    void* tree;
     FILE* qrytxt;
+    FILE* qrysvg;
 };
+
+void shotHit(Info i, double x, double y, double mbbX1, double mbbY1, double mbbX2, double mbbY2, void* aux)
+{
+    struct d* d = aux;
+    if (getFormType(i) == RECTANGLE)
+    {
+        if (d->shootX >= x && d->shootY >= y && d->shootX <= x + getFormW(i) && d->shootY <= y + getFormH(i))
+        {
+            d->hit = true;
+            fprintf(d->qrytxt, "Shot hit ship %d (%.2lf, %.2lf, %.2lf, %.2lf)\n", getFormId(i), x, y, getFormW(i), getFormH(i));
+            removeSRb(d->tree, x, y, 0, 0, 0, 0);
+        }
+    }
+}
+
+void calculateShootPos(char* side, double d, double x, double y, double w, double h, double* xref, double* yref)
+{
+    if (!strcmp(side, "PP")) // Popa
+    {
+        *xref = x + (w / 2);
+        *yref = y - d;
+    }
+    else if (!strcmp(side, "BB")) // Bombordo
+    {
+        *xref = x + w + d;
+        *yref = y + (h / 2);
+    }
+    else if (!strcmp(side, "PR")) // Proa
+    {
+        *xref = x + (w / 2);
+        *yref = y + h + d;
+    }
+    else if (!strcmp(side, "EB")) // Estibordo
+    {
+        *xref = x - d;
+        *yref = y + (h / 2);
+    }
+}
+
+void shoot(Info i, double x, double y, double mbbX1, double mbbY1, double mbbX2, double mbbY2, void* aux)
+{
+    struct d* d = aux;
+    if (getFormId(i) == d->id)
+    {
+        if (getFormType(i) == RECTANGLE)
+        {
+            if (getNauEnergy(i) >= d->d)
+            {
+                setNauEnergy(i, getNauEnergy(i) - d->d);
+                calculateShootPos(d->side, d->d, x, y, getFormW(i), getFormH(i), &d->shootX, &d->shootY);
+                fprintf(d->qrytxt, "Shot at (%.2lf, %.2lf)\n", d->shootX, d->shootY);
+                fprintf(d->qrysvg, TEXT_SVG, -1, d->shootX, d->shootY, "black", "black", "middle", "*");
+                percursoProfundidade(d->tree, shotHit, aux);
+                if (!d->hit)
+                {
+                    fprintf(d->qrytxt, "Shot missed.\n");
+                }
+            }
+            else
+            {
+                fprintf(d->qrytxt, "This ship has no energy to shoot.\n");
+            }
+        }
+    }
+}
 
 void d(SRbTree tree, FILE* qry, FILE* qrytxt, FILE* qrysvg)
 {
     struct d* aux = calloc(1, sizeof(struct d));
-    aux->qrysvg = qrysvg;
+    aux->tree = tree;
     aux->qrytxt = qrytxt;
+    aux->qrysvg = qrysvg;
+    aux->hit = false;
     fscanf(qry, "%d %s %lf", &aux->id, aux->side, &aux->d);
-    
+    fprintf(qrytxt, "\n>The ship %d will shoot in the %s side with a distance of %.2lf:\n", aux->id, aux->side, aux->d);
+    percursoProfundidade(tree, shoot, aux);
     free(aux);
 }
 
